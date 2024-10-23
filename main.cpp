e649d045cfead62869aa009c66c98334ddabf26e
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-10-23 21:07:29 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,86 @@
 #include <iostream>
 using namespace std;
-
+ESERCIZIO 1
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+   int i, n, contpari, contdispari, restodue, x;
+do {
+    cout<<"quanti numeri da generare?";
+    cin>>n;
+} while (n<0);
+contpari=0;
+contdispari=0;
+i=0;
+for (i=0; i<= n; i++) {
+    x=rand() % 1001;
+    restodue=x%2;
+    if(restodue==0) {
+        contpari=contpari+1;
+    } else {
+        contdispari=contdispari+1;
+    }
+    cout <<i+1<<"numero generato"<<x<<;
 }
+cout<<"sono stati generati"<<contpari<<"numeri pari e"<<contdispari<<"numeri dispari";
+}
+ESERCIZIO 2
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    string nome;
+    int i, n;
+    cout << "Quanti atleti ci sono in gara?" << endl;
+    cin >> n;
+    for (i = 0; i <= n; i++) {
+        cout << "Inserisci il nome dell'atleta" << endl;
+        cin >> nome;
+        cout << "ciao" << nome << endl;
+        }
+    
+    ESERCIZIO 3
+    #include <iostream>
+    using namespace std;
+    int n, m, i;
+    cout << "elenco i numeri da N a M con passo 2" << endl;
+    cout << "Inserisci N" << endl;
+    cin >> n;
+    cout << " inserisci M" << endl;
+    cin >> m;
+    for  (i = m; i <= n; i += 2) {
+        cout  << i  << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
